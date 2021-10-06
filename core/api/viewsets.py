1dc94d52df323b469fee56ea527a0ca59e42f03f
unknown <eduardoeandroid@gmail.com>
2021-10-06 14:08:28 -0300

    Autenticacao

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -4,10 +4,14 @@ from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, DjangoModelPermissions
+from rest_framework.authentication import TokenAuthentication
 
 class PontoTuristicoViewSet(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
     filter_backends = (SearchFilter, )
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, ]
     search_fields = ('nome', 'descricao', 'endereco__linha1')
     """
     ^ => começa com o valor passado
--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,4 +23,5 @@ router.register(r'avaliacoes', AvaliacaoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-token-auth/', obtain_auth_token),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
